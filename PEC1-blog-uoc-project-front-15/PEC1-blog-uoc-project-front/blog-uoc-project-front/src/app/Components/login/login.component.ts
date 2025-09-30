5c994a69493ffb51a15bbf61c92174a7004a16e7
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-09-30 13:10:32 +0200

    ej 2

--- PEC1-blog-uoc-project-front-15/PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Components/login/login.component.ts
+++ PEC1-blog-uoc-project-front-15/PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Components/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthDTO } from 'src/app/Models/auth.dto';
+import { HeaderMenus } from 'src/app/Models/header-menus.dto';
 import { AuthService } from 'src/app/Services/auth.service';
 import { HeaderMenusService } from 'src/app/Services/header-menus.service';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
@@ -11,14 +13,12 @@ import { SharedService } from 'src/app/Services/shared.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  /*
+export class LoginComponent {
   // TODO 19
   loginUser: AuthDTO;
   email: FormControl;
   password: FormControl;
   loginForm: FormGroup;
-  */
 
   constructor(
     private formBuilder: UntypedFormBuilder,
@@ -28,13 +28,16 @@ export class LoginComponent implements OnInit {
     private localStorageService: LocalStorageService,
     private router: Router
   ) {
-    // TODO 20
+    this.loginUser = new AuthDTO('', '', '', '');
+    this.email = new FormControl(this.loginUser.email, [Validators.required, Validators.email]);
+    this.password = new FormControl(this.loginUser.password, [Validators.required, Validators.minLength(8), Validators.maxLength(16)]);
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password: this.password
+    })
   }
 
-  ngOnInit(): void {}
-
   async login(): Promise<void> {
-    /*
     let responseOK: boolean = false;
     let errorResponse: any;
 
@@ -75,6 +78,5 @@ export class LoginComponent implements OnInit {
       this.headerMenusService.headerManagement.next(headerInfo);
       this.router.navigateByUrl('home');
     }
-    */
   }
 }
