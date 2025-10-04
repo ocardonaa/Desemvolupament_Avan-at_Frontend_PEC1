bcae82ec30a37d97c398c3ea072b442c4eca53cf
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-10-04 13:29:51 +0200

    ej 8

--- PEC1-blog-uoc-project-front-15/PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Pipes/format-date.pipe.ts
+++ PEC1-blog-uoc-project-front-15/PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Pipes/format-date.pipe.ts
@@ -4,8 +4,31 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatDate',
 })
 export class FormatDatePipe implements PipeTransform {
+
+  private addZeros(dateNumber: string): string {
+    if(dateNumber.length === 1) {
+      return '0' + dateNumber;
+    }
+    else return dateNumber;
+  }
+
   transform(value: Date, ...args: number[]): unknown {
     // TODO 1
-    return null;
+    let newDate = new Date(value).toLocaleDateString();
+    const sepDate = newDate.split("/");
+    const correctMonth = this.addZeros(sepDate[0]);
+    const correctDay = this.addZeros(sepDate[1]);
+    switch (args[0]) {
+      case 1:
+        return correctDay + correctMonth + sepDate[2];
+      case 2:
+        return correctDay + ' / ' + correctMonth + ' / ' + sepDate[2];
+      case 3:
+        return correctDay + '/' + correctMonth + '/' + sepDate[2];
+      case 4:
+        return sepDate[2] + '-' + correctMonth + '-' + correctDay;
+      default:
+        return null;
+    }
   }
 }
